f7a9bc797cce7a207fe266537f10af77148a2ee0
Lupino <lmjubuntu@gmail.com>
2017-08-14 13:34:42 +0800

    FileService: add get remove put

--- service/src/FileService.js
+++ service/src/FileService.js
@@ -1,8 +1,16 @@
 import Gateway from 'yuntan-gateway';
 
 export class FileService extends Gateway {
-  save(fileName, raw) {
+  put(fileName, raw) {
     const pathname = `/file/${fileName}`;
     return this.request({ method: 'PUT', pathname, raw });
+  }
+  get(fileName) {
+    const pathname = `/file/${fileName}`;
+    return this.request({ pathname });
+  }
+  remove(fileName) {
+    const pathname = `/file/${fileName}`;
+    return this.request({ method: 'DELETE', pathname });
   }
 }
