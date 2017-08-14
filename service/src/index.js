8cfd1d94c8597af100f3627aa712cacdecd47e18
Lupino <lmjubuntu@gmail.com>
2017-08-14 14:52:41 +0800

    service: fixed service import

--- service/src/index.js
+++ service/src/index.js
@@ -1,8 +1,8 @@
-import CoinService from 'CoinService';
-import FileService from 'FileService';
-import FuncService from 'FuncService';
-import UserService from 'UserService';
-import ArticleService from 'ArticleService';
+import CoinService from './CoinService';
+import FileService from './FileService';
+import FuncService from './FuncService';
+import UserService from './UserService';
+import ArticleService from './ArticleService';
 
 export {
   CoinService,
