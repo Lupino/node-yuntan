3cc367d36efc920268f371faebc3d111f64864c4
Lupino <lmjubuntu@gmail.com>
2017-08-17 17:23:13 +0800

    service: update func service doc

--- service/src/FuncService.js
+++ service/src/FuncService.js
@@ -3,12 +3,17 @@ import Gateway from 'yuntan-gateway';
 /**
  * Function as a service
  * @class FuncService
+ * @param {Object} config Service config
+ * @param {String} [config.host=https://gw.huabot.com] Service host
+ * @param {String} config.key Service key
+ * @param {String} [config.secret] Service secret
+ * @param {signSecret} [config.signSecret] dynamic secret sign function
  */
 export default class FuncService extends Gateway {
   /**
    * Run a function
    * @async
-   * @function
+   * @function FuncService::run
    * @param {json} options
    * @return {Response}
    */
